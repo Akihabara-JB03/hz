44051b5b81a3f55bf61e20bbf465d72eb680855c
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-11 17:57:12 +0900

    Update hz.go

--- src/hz.go
+++ src/hz.go
@@ -2,6 +2,7 @@ package main
 import (
   "fmt"
   "os"
+  "io"
 )
 func tukaikata() {
       fmt.Println("使い方をしれよ！！")
@@ -10,18 +11,46 @@ func tukaikata() {
 }
 func pack(input string,output string) {
   file,err := os.Create(output)
+  
   if err != nil {
     fmt.Printf("圧縮ファイル作成エラーなり！！　詳細はこれなり！！ エラー内容:%s",err)
     return
     
   }
+  defer file.Close()
   inpfile,err := os.Open(input)
+  
+  
+  
   if err != nil {
-    fmt.Printf("圧縮元ファイル確認エラーなり！！　詳細はこれなり！！　エラー内容:%s",err)
+    fmt.Printf("圧縮元ファイル確認準備エラーなり！！　詳細はこれなり！！　エラー内容:%s",err)
     return
   }
-  defer file.Close()
   defer inpfile.Close()
+  data, err := io.ReadAll(inpfile)
+  if err != nil {
+    fmt.Printf("圧縮元ファイル確認エラーなり！！詳細はこれなり！！　エラー内容:%s",err)
+    return
+  }
+  count := 1
+  s := string(data)
+  for i := 0; i < len(s); i++{
+    if i+1 < len(s) && s[i] == s[i+1] {
+      // 同じ文字
+      count++
+    } else {
+      if count >= 5 {
+        
+      } else {
+
+      }
+      count = 0
+    }
+  }
+  file.Write(data)
+  
+  
+
 }
 func main() {
   if len(os.Args) < 4 {
