9a8ad02b39afb3815449d11fe037ada7bad28e90
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-07-12 10:51:29 +0900

    Update hz.go

--- src/hz.go
+++ src/hz.go
@@ -40,20 +40,68 @@ func pack(input string,output string) {
       count++
     } else {
       if count >= 5 {
-        
+        fmt.Fprintf(file, "%c{%d}", s[i], count)
       } else {
-
+        for j := 0; j < count; j++ {
+          fmt.Fprintf(file, "%c", s[i])
+        }
       }
-      count = 0
+      count = 1
     }
+   
   }
-  if count >= 5 {
-        
-  }
-  file.Write(data)
-  
-  
+   fmt.Println("圧縮完了なり！！")
+}
+func unpack(input string, output string) {
+	file, err := os.Create(output)
+	if err != nil {
+		fmt.Printf("展開ファイル作成エラーなり！！ エラー内容:%s\n", err)
+		return
+	}
+	defer file.Close()
 
+	inpfile, err := os.Open(input)
+	if err != nil {
+		fmt.Printf("圧縮ファイル確認準備エラーなり！！ エラー内容:%s\n", err)
+		return
+	}
+	defer inpfile.Close()
+
+	data, err := io.ReadAll(inpfile)
+	if err != nil {
+		fmt.Printf("データ読み込みエラーなり！！ エラー内容:%s\n", err)
+		return
+	}
+
+	s := string(data)
+
+	for i := 0; i < len(s); i++ {
+		// もし次の文字が '{' だったら、カッコの中の数字を読み取る
+		if i+1 < len(s) && s[i+1] == '{' {
+			currentChr := s[i] // 繰り返したい文字（例: 'A'）
+
+			var count int
+			var readLen int
+			// カッコの中の数字（例: 300）と、その文字数（readLen）を同時に取得
+			_, err := fmt.Sscanf(s[i+1:], "{%d}%n", &count, &readLen)
+
+			if err == nil {
+				// 読み取れた回数分、その文字をファイルに書き出す
+				for j := 0; j < count; j++ {
+					fmt.Fprintf(file, "%c", currentChr)
+				}
+				// カッコの分（ {300} など）だけインデックスをスキップ
+				i += readLen
+			} else {
+				// 万が一読み取りに失敗したらそのまま1文字として書き出す
+				fmt.Fprintf(file, "%c", s[i])
+			}
+		} else {
+			// 普通の文字はそのまま1文字書き出す
+			fmt.Fprintf(file, "%c", s[i])
+		}
+	}
+	fmt.Println("展開が完了したなり！")
 }
 func main() {
   if len(os.Args) < 4 {
@@ -63,7 +111,7 @@ func main() {
     case "pack":
       pack(os.Args[2],os.Args[3])
     case "unpack":
-
+      unpack(os.Args[2],os.Args[3])
     default:
       tukaikata()
   }
